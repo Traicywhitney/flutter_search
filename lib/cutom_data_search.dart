396252559c36a6e805ac6d4e25cf745dcb4faf8b
Traicy Whitney <whitneytraicy2001@gmail.com>
2024-04-25 17:46:32 +0530

    initial commit

---
+++ lib/cutom_data_search.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class DataSearch extends SearchDelegate<String> {
+  final cities = [
+    'City 1',
+    'City 2',
+    'City 3',
+    // Add as many city names as you need
+  ];
+
+  final recentCities = [
+    'City 1',
+    'City 2',
+  ];
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: Icon(Icons.clear),
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container(
+      width: 100.0,
+      height: 100.0,
+      child: Card(
+        color: Colors.red,
+        child: Center(
+          child: Text(query),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestionList = query.isEmpty
+        ? recentCities
+        : cities.where((c) => c.startsWith(query)).toList();
+
+    return ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+        onTap: () {
+          showResults(context);
+        },
+        leading: Icon(Icons.location_city),
+        title: RichText(
+          text: TextSpan(
+            text: suggestionList[index].substring(0, query.length),
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.bold),
+            children: [
+              TextSpan(
+                text: suggestionList[index].substring(query.length),
+                style: TextStyle(color: Colors.grey),
+              ),
+            ],
+          ),
+        ),
+      ),
+      itemCount: suggestionList.length,
+    );
+  }
+}
